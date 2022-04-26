559ec80bfebed2aa49a9d62456130b225ccf6e15
Yunho Kim <31121701+Kandreaop@users.noreply.github.com>
2022-04-26 21:25:19 +0900

    ModifyVariables_2

--- AlgorithmTermProject/src/Mypackage/Login.java
+++ AlgorithmTermProject/src/Mypackage/Login.java
@@ -187,14 +187,6 @@ public class Login {
 		JButton resister_btn;
 		JTextArea content;
 
-		JFrame sub = new JFrame("Resister");
-		JLabel lbl, namelbl, gradelbl, emaillbl, idlbl, pwlbl, stnumlbl;
-		JTextField nameField, gradeField, emailField, idField, stnumField;
-		JPasswordField passwd;
-		JPanel namePanel, gradePanel, emailPanel, idPanel, paPanel, stnumPanel;
-		JButton resister_btn;
-		JTextArea content;
-
 		public Resister() {
 
 			sub.setLayout(new FlowLayout());
