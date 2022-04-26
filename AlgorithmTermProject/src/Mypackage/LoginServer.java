590bec0d72b8fdf280c5c4a7bbde0a0b46979eb3
jomo34 <trillion23@naver.com>
2022-04-26 21:27:16 +0900

    Modify complete

--- AlgorithmTermProject/src/Mypackage/LoginServer.java
+++ AlgorithmTermProject/src/Mypackage/LoginServer.java
@@ -22,9 +22,9 @@ import java.util.concurrent.Executors;
 
 public class LoginServer
 {
-   private static Set<String> userlist = new HashSet<>(); //媛��엯�옄�뱾�쓽 �젙蹂� ���옣.
+   private static Set<String> userlist = new HashSet<>(); //가입자들의 정보 저장
    private static Set<PrintWriter> writers = new HashSet<>(); //for broadcast
-   private static Set<String> chatList = new HashSet<>(); //chat�궗�슜�옄�뱾 由ъ뒪�듃愿�由�
+   private static Set<String> chatList = new HashSet<>(); //chat사용자들 리스트관리
    private static String [][] time1 = new String [255][3];
    private static String [][] table1 = new String [255][3];
    private static int countT1 = 0;
@@ -46,7 +46,7 @@ public class LoginServer
       }
    }
    public static void activity(String time1[][],int N ) {
-		// �걹�굹�뒗 �떆媛꾩쓣 湲곗��쑝濡� �젙�젹
+		// 끝나는 시간을 기준으로 정렬
 	   countT1=0;
 	 
 	   Arrays.sort(time1, new Comparator<String[]>() {      
@@ -65,7 +65,7 @@ public class LoginServer
 	
 		for(int i = 0; i < N; i++) {
 			
-			// 吏곸쟾 醫낅즺�떆媛꾩씠 �떎�쓬 �쉶�쓽 �떆�옉 �떆媛꾨낫�떎 �옉嫄곕굹 媛숇떎硫� 媛깆떊 
+			// 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 실행
 			if(prev_end_time <= Integer.parseInt(time1[i][0])) {
 				prev_end_time =  Integer.parseInt(time1[i][1]);
 				table1[countT1][0] = time1[i][0];
@@ -79,13 +79,13 @@ public class LoginServer
  }
    public static String Converter(String id)
    {
-      String DB_URL = "jdbc:mysql://localhost:3306/algorithm?serverTimezone=UTC"; //�젒�냽�븷 DB �꽌踰�
-       Connection conn=null;//mysql �젒�냽�슜
-       Statement state=null;//mysql �뿰寃�
+      String DB_URL = "jdbc:mysql://localhost:3306/algorithm?serverTimezone=UTC"; //접속할 DB 저버
+       Connection conn=null;//mysql 접속용
+       Statement state=null;//mysql 연결
        String uname=null;
        try {
-          conn=DriverManager.getConnection(DB_URL,"root","990302"); //db二쇱냼��, �궗�슜�옄, 鍮꾨�踰덊샇瑜� �넻�빐�꽌 �젒洹�.
-          state=conn.createStatement(); //mysql �뿰寃�
+          conn=DriverManager.getConnection(DB_URL,"root","990302"); //db주소와, 사용자, 비밀번호를 통해서 접근
+          state=conn.createStatement(); //mysql 연결
           
           try {
              String sqlname="select name from user where userid like"+"'"+id+"'";
@@ -115,10 +115,10 @@ public class LoginServer
       private PrintWriter out;
       private String pw;
       int schedulecount=0;
-      private String logid=""; //濡쒓렇�븘�씠�뵒 ���옣 蹂��닔
-       String DB_URL = "jdbc:mysql://localhost:3306/algorithm"; //�젒�냽�븷 DB �꽌踰�
-         Connection conn=null;//mysql �젒�냽�슜
-         Statement state=null;//mysql �뿰寃�
+      private String logid=""; //로그아이디 저장 변수
+       String DB_URL = "jdbc:mysql://localhost:3306/algorithm"; //접속할 DB서버
+         Connection conn=null;//mysql 접속용
+         Statement state=null;//mysql 연결
       public Handler(Socket socket)
       {
          this.socket = socket;
@@ -133,24 +133,18 @@ public class LoginServer
          String time=curtime.getCurrentTime();
          String [] scheduler=time.split(":");
          Class.forName("com.mysql.cj.jdbc.Driver"); //mysql driver
-         conn=DriverManager.getConnection(DB_URL,"root","990302"); //db二쇱냼��, �궗�슜�옄, 鍮꾨�踰덊샇瑜� �넻�빐�꽌 �젒洹�.
-         state=conn.createStatement(); //mysql �뿰寃�
-         boolean realuser=false; //�떎�젣 �궗�슜�옄媛� �엳�뒗 吏� �솗�씤�븯湲� �쐞�븳 蹂��닔
-         //Statement state9=conn.createStatement();
-         //String list="select userid from info"; 
-         //ResultSet ulist=state9.executeQuery(list);//�뜲�씠�꽣踰좎씠�뒪�쓽 userid瑜� �떎 媛��졇�샂.
-         //while(ulist.next())
-         //{
-           // userlist.add(ulist.getString("userid"));//�뜲�씠�꽣踰좎씠�뒪�뿉�꽌 媛��졇�삩 userid瑜�  server�쓽 hashlist�뿉���옣.
-         //}
+         conn=DriverManager.getConnection(DB_URL,"root","990302"); //db주소와, 사용자, 비밀번호를 통해서 접근
+         state=conn.createStatement(); //mysql 연결
+         boolean realuser=false; //실제 사용자가 있는지 확인하기 위한 변수
+
          while(true)
          {
-         String input=in.nextLine();//�냼耳볦뿉�꽌 �삩 �젙蹂대�� �씫�뼱�샂
+         String input=in.nextLine();//소켓에서 온 정보를 읽어옴
        
          System.out.println(input);
-            if(input.startsWith("logid")) //client�뿉�꽌 logid媛� �솕�쓣 �븣, 濡쒓렇�씤
+            if(input.startsWith("logid")) //client에서 logid가 왔을때 로그인
             {
-               String[]info=input.split(" "); //id�� password媛� 媛숈씠���꽌 �굹�닠以�.
+               String[]info=input.split(" "); //id와 password가 같이와서 나눠줌
                id=info[0].substring(5); //id
                pw=info[1]; //password
                
@@ -158,25 +152,25 @@ public class LoginServer
                ResultSet rsi=state.executeQuery(sqlid);
                if ((rsi.next() == false || (id.isEmpty()) == true))
                {
-                  out.println("id invalid"); //濡쒓렇�씤 �떎�뙣�씪怨� �겢�씪�씠�뼵�듃�뿉寃� 蹂대궡以�
+                  out.println("id invalid"); //로그인 실패라고 클라이언트에게 보내줌
                }
                rsi.close();
                Statement state2=conn.createStatement();
-               String sql="select * from user where userid like"+"'"+id+"'"; //id瑜� 諛뷀깢�쑝濡� 鍮꾨�踰덊샇瑜� 李얠쓬
-               ResultSet rs=state2.executeQuery(sql); //寃곌낵 媛� ���옣.
+               String sql="select * from user where userid like"+"'"+id+"'"; //id를 바탕으로 비밀번호를 찾음
+               ResultSet rs=state2.executeQuery(sql); //결과값 저장
                Statement state3=conn.createStatement();
            
-               while(rs.next()) //�뜲�씠�꽣踰좎씠�뒪 �뒪罹�
+               while(rs.next()) //데이터베이스 스캔
                {
-                  String dbpw=rs.getString("password"); //議곌굔臾몄뿉 留욌뒗 �뙣�뒪�썙�뱶 ���옣
+                  String dbpw=rs.getString("password"); //조건문에 맞는 패스워드 저장
                   
-                  if(pw.equalsIgnoreCase(dbpw)) //�엯�젰�븳 鍮꾨�踰덊샇�� �븘�씠�뵒媛� 留욎쓣 �븣, 濡쒓렇�씤�꽦怨�
+                  if(pw.equalsIgnoreCase(dbpw)) //입력한 비밀번호와 아이디가 맞을 때, 로그인 성공
                   {
-                     out.println("access:"+id+":"+rs.getInt("stnumber")+":"+rs.getString("email")+":"+rs.getString("name")+":"+rs.getInt("grade"));//�겢�씪�씠�뼵�듃�뿉寃� �꽦怨듯뻽�떎怨� 蹂대궡以�.
+                     out.println("access:"+id+":"+rs.getInt("stnumber")+":"+rs.getString("email")+":"+rs.getString("name")+":"+rs.getInt("grade"));//클라이언트에게 성공했다고 보내줌
                   }
                   else
                   {
-                  out.println("pw invalid"); //濡쒓렇�씤 �떎�뙣   
+                  out.println("pw invalid"); //로그인 실패 
                   }   
                   }
                rs.close();
@@ -184,9 +178,9 @@ public class LoginServer
                state2.close();
                writers.add(out);
                }
-            else if(input.startsWith("Resister")) //�쉶�썝媛��엯
+            else if(input.startsWith("Resister")) //회원가입
             {
-               String [] nw=input.split(" "); //�쉶�썝媛��엯�릺�꽌 �삩 �젙蹂대�� �굹�닠以�.
+               String [] nw=input.split(" "); //회원가입되서 온 정보를 나눠줌
                String addname=nw[0].substring(8);
                int addstnum=Integer.parseInt(nw[1]);
                String addemail=nw[2];
@@ -195,26 +189,25 @@ public class LoginServer
                int addgrade = Integer.parseInt(nw[5]);
                Statement state4=conn.createStatement();
                String mysql="insert into user values("+"'"+addid+"'"+","+"'"+addpw+"'"+","+addstnum+","+"'"+addemail+"'"+","+"'"+addname+"'"+","+addgrade+")";
-               //String mysql="insert into info values("+"'"+addname+"'"+","+addbday+","+"'"+addemail+"'"+","+"'"+addid+"'"+","+"'"+addpw+"'"+","+0+","+"'"+""+"'"+","+"'"+"hello world"+"'"+")";
                   
-               //�뜲�씠�꽣踰좎씠�뒪�뿉 ���옣�떆耳쒖＜�뒗 援щЦ
-               state4.executeUpdate(mysql); //�뜲�씠�꽣踰좎씠�뒪 �뾽�뜲�씠�듃
-               userlist.add(nw[3]); //媛��엯�븯硫�, hashset�뿉 userid 異붽�
-               out.println("Welcome"); //�쉶�썝媛��엯 �릺�뿀�떎�뒗 寃껋쓣 蹂대궡以�.
+               //데이터베이스에 저장시켜주는 구문
+               state4.executeUpdate(mysql); //데이터베이스 업데이트
+               userlist.add(nw[3]); //가입하면, hashset에 userid추가
+               out.println("Welcome"); //회원가입되었다는것을 보내줌
                state4.close();
             }
-            else if(input.startsWith("Update")) {  // �궡�젙蹂� 蹂�寃� �븷 �븣 
+            else if(input.startsWith("Update")) {  // 내정보 변경
                String[] ch_info = input.split(":");
                try {
-	               String ch_sql = "update user set email = " + "'"+ch_info[2]+"',"+"grade = "+ch_info[3]+" where userid = " +"'"+ch_info[1]+"'";// �땳�꽕�엫�씠�옉 �긽�깭硫붿떆吏� 蹂�寃쏀븿
+	               String ch_sql = "update user set email = " + "'"+ch_info[2]+"',"+"grade = "+ch_info[3]+" where userid = " +"'"+ch_info[1]+"'";// 닉네임과 상태메시지 변경
 	               Statement state10 = conn.createStatement();
 	               
 	               state10.executeUpdate(ch_sql);
 	
 	               state10.close();
-	               out.println("UC"); // �뾽�뜲�씠�듃 �꽦怨듯뻽�떎怨� 蹂대궡以�. 
+	               out.println("UC"); // 업데이트 성공했다고 보내줌
                }
-               // update �떎�뙣�떆,
+               // update 실패시
                catch(Exception e)
                {
             	   out.println("UF");
@@ -222,13 +215,13 @@ public class LoginServer
                }
                
             }
-            else if(input.startsWith("Book"))// �삁�빟諛쏆븘�꽌 ���옣�븯寃뚮걫 �븳�떎.
+            else if(input.startsWith("Book"))// 예약받아서 저장
             {
             	 Calendar now = Calendar.getInstance();
                  int h= now.get(Calendar.HOUR_OF_DAY);
             	String [] bookingList=input.split(" ");
-            	 // �삁�빟�옄 �븘�씠�뵒
-            	if(h<18) // 6�떆 �씠�쟾�뿉留� �삁�빟�쓣 諛쏅뒗�떎. 
+            	 // 예약자 아이디
+            	if(h<18) // 6시 이전에만 예약을 받는다.
             	{
                	int a = Integer.parseInt(bookingList[1]);
                	int b = Integer.parseInt(bookingList[2]);
@@ -237,8 +230,8 @@ public class LoginServer
             	
             	
             	        	
-				time1[N][0]=String.valueOf(c);//�떆�옉�떆媛� 
-    			time1[N][1]=String.valueOf(d);//醫낅즺�떆媛� 
+				time1[N][0]=String.valueOf(c);//시작시간
+    			time1[N][1]=String.valueOf(d);//종료ㅕ시간
     			time1[N][2]=bookingList[3];//ID
     			System.out.println("start: " + time1[N][0] + " end: "+ time1[N][1]+ " id: "+ time1[N][2]);
     			N++;
@@ -258,7 +251,7 @@ public class LoginServer
 			
 			int a=0;
 
-			if(Integer.parseInt(scheduler[0])==15 && schedulecount==0 && Integer.parseInt(scheduler[1])==37) // 6�떆 �릺硫� �븳踰덈쭔 吏쒖��떎. 
+			if(Integer.parseInt(scheduler[0])==15 && schedulecount==0 && Integer.parseInt(scheduler[1])==37) // 6시 되면 한번만 돌아감 
 			{
 				activity(time1,N);
 					for(int i=0;i<countT1;i++) {
@@ -276,7 +269,7 @@ public class LoginServer
               		schedulecount++;
 				}
 			}
-			else if(input.startsWith("Time")) //�뒪耳�以꾩쓣 吏쒖＜�뒗 寃쎌슦, 諛붽퓭�빞�븳�떎. 
+			else if(input.startsWith("Time")) //스케줄을 짜주는 경우, 바꿔야한다
 			{
 				
 				
